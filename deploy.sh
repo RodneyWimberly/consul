92f48392af63b1946524888cda8ae6dc9234658c
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-19 23:54:42 -0800

    More Fixes

--- deploy.sh
+++ deploy.sh
@@ -23,16 +23,22 @@ export NODE_IS_MANAGER=$(docker info --format "{{.Swarm.ControlAvailable}}")
 echo "Node Is Manager: ${NODE_IS_MANAGER}"
 
 echo " --> Validating swarm network infrastructure"
-NET_ID=$(docker network ls -f name=consul_network -q)
+NET_ID=$(docker network ls -f name=admin_network -q)
 if [[ -z "$NET_ID" ]]; then
-    echo "Creating attachable overlay network 'consul_network'"
+    echo "Creating attachable overlay network 'admin_network'"
     set +e
-    docker network create --driver=overlay --attachable --subnet=192.168.1.0/24 consul_network
+    docker network create --driver=overlay --attachable --subnet=192.168.1.0/24 admin_network
     set -e
 fi
 
 echo " --> Deploying DevOps Stack to Swarm"
-docker stack deploy --compose-file=./devops-stack.yml devops
+docker stack deploy --compose-file=./stacks/devops-stack.yml devops
+
+echo " --> Deploying Logging Stack to Swarm"
+docker stack deploy --compose-file=./stacks/logging-stack.yml logging
 
 echo " --> Deploying Consul Stack to Swarm"
-docker stack deploy --compose-file=./consul-stack.yml "${CONSUL_STACK_PROJECT_NAME}"
+docker stack deploy --compose-file=./stacks/consul-stack.yml "${CONSUL_STACK_PROJECT_NAME}"
+
+echo " --> Deploying Diagnostics Stack to Swarm"
+docker stack deploy --compose-file=./stacks/diagnostics-stack.yml diagnostics
--- get-latest.sh
+++ get-latest.sh
@@ -9,14 +9,3 @@ git pull --progress "origin" DeploymentTest:DeploymentTest
 chmod u+x *.sh
 chmod u+x ./scripts/*.sh
 ./deploy.sh
-
-#echo "${GITHUB_ACCESS_TOKEN}" > key.txt
-#gh auth login --with-token < key.txt
-#rm -rf /tmp/consul
-#echo "Cloning Consul repo"
-#rm -rf /tmp/consul
-#gh repo clone RodneyWimberly/consul /tmp/consul -b DevelopmentTest
-#cd /tmp/consul
-#chmod u+x *.sh
-#chmod u+x ./scripts/*.sh
-#./deploy.sh
