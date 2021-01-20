3870c27d3f77c135edbf70b0ad7391df50f6a32a
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-20 10:23:35 -0800

    More fixes

--- bootstrap-deployment.sh
+++ bootstrap-deployment.sh
@@ -1,5 +1,17 @@
 # Install GitHub CLI (curl), Clone Consul repo, and run deployment script.
 # This can be used to build application stacks on bare containers
+apk add git && \
+cd /tmp && \
+rm -rf /tmp/consul && \
+git clone -b DeploymentTest \
+    https://rodneywimberly:b606a0781f57605d4e5b00b753a6f26c23ff8908@github.com/RodneyWimberly/consul.git \
+    /tmp/consul && \
+cd /tmp/consul && \
+chmod u+x *.sh && \
+chmod u+x ./scripts/*.sh && \
+./deploy.sh
+
+
 GITHUB_ACCESS_TOKEN=b606a0781f57605d4e5b00b753a6f26c23ff8908 && \
 VERSION=`curl  "https://api.github.com/repos/cli/cli/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c2-` && \
 echo "Downloading GitHub CLI version "$VERSION && \
@@ -25,18 +37,4 @@ chmod u+x ./scripts/*.sh && \
 
 
 
-echo "Adding Git" && \
-apk update && \
-apk add git && \\
-echo "Cloning Consul repo" && \
-rm -rf /tmp/consul && \
-git clone -b DeploymentTest \
-    https://rodneywimberly:b606a0781f57605d4e5b00b753a6f26c23ff8908@github.com/RodneyWimberly/consul.git \
-    /tmp/consul && \
-cd /tmp/consul && \
-chmod u+x *.sh && \
-chmod u+x ./scripts/*.sh && \
-./deploy.sh
 
-
-https://github.com/RodneyWimberly/consul.git
