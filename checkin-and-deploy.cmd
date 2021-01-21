1507a708c785c58f83c61ae4fba20c470dba2ec2
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-21 11:56:14 -0800

    <your message here>

--- checkin-and-deploy.cmd
+++ checkin-and-deploy.cmd
@@ -1,4 +1,5 @@
 @echo off
+cls
 cd d:\consul\soakes\consul
 
 echo " --> Adding changed items to source control"
@@ -11,6 +12,6 @@ echo " --> Pulling changed items from source control"
 git pull --force --progress https://rodneywimberly:b606a0781f57605d4e5b00b753a6f26c23ff8908@github.com/RodneyWimberly/consul.git
 
 echo " --> Executing update-stack.sh on Worker1"
-start putty.exe -load Worker1
+start "Worker1 Update" putty.exe -load Worker1
 echo " --> Executing update-stack.sh on Manager1"
-start putty.exe -load Manager1
+start "Manager1 Update" putty.exe -load Manager1
