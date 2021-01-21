78d15c7cf10713827fe39150a44e292e2b89a999
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-21 10:58:31 -0800

    <your message here>

--- checkin-and-deploy.cmd
+++ checkin-and-deploy.cmd
@@ -3,8 +3,11 @@ cd d:\consul\soakes\consul
 
 echo "Check-in and sync changes"
 git add .
+echo "1"
 git commit -m "<your message here>"
+echo "2"
 git push --progress --all --repo=https://rodneywimberly:b606a0781f57605d4e5b00b753a6f26c23ff8908@github.com/RodneyWimberly/consul.git
+echo "3"
 git pull --force --progress https://rodneywimberly:b606a0781f57605d4e5b00b753a6f26c23ff8908@github.com/RodneyWimberly/consul.git
 
 echo "Executing update-stack.sh on Worker1"
