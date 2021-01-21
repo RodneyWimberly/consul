1e18a9dc48c6f806f221115a037f519390bcf933
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-21 11:53:33 -0800

    <your message here>

--- checkin-and-deploy.cmd
+++ checkin-and-deploy.cmd
@@ -1,16 +1,16 @@
 @echo off
 cd d:\consul\soakes\consul
 
-echo "Check-in and sync changes"
+echo " --> Adding changed items to source control"
 git add .
-echo "1"
+echo " --> Commiting changed items to source control"
 git commit -m "<your message here>"
-echo "2"
+echo " --> Pushing changed items to source control"
 git push --progress --all --repo=https://rodneywimberly:b606a0781f57605d4e5b00b753a6f26c23ff8908@github.com/RodneyWimberly/consul.git
-echo "3"
+echo " --> Pulling changed items from source control"
 git pull --force --progress https://rodneywimberly:b606a0781f57605d4e5b00b753a6f26c23ff8908@github.com/RodneyWimberly/consul.git
 
 echo "Executing update-stack.sh on Worker1"
-start "putty.exe" "-load Worker1"
+start "putty.exe -load Worker1"
 echo "Executing update-stack.sh on Manager1"
-start "putty.exe" "-load Manager1"
+start "putty.exe -load Manager1"
