7d6cb1f3421820e1696329b603b8eff480c7d4c7
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-21 10:58:31 -0800

    <your message here>

--- bootstrap-deployment.sh
+++ bootstrap-deployment.sh
@@ -11,7 +11,7 @@ apk add git
 cd /tmp
 rm -rf /tmp/consul
 git clone -b DeploymentTest \
-    https://rodneywimberly:b606a0781f57605d4e5b00b753a6f26c23ff8908@github.com/RodneyWimberly/consul.git \
+      \
     /tmp/consul
 cd /tmp/consul
 chmod u+x *.sh
@@ -20,8 +20,6 @@ chmod u+x ./scripts/*.sh
 exit
 EOL
 chmod u+x update-stack.sh
-
-
 
 # Worker Script
 touch update-stack.sh && \
@@ -37,3 +35,16 @@ git clone -b DeploymentTest \
 exit
 EOL
 chmod u+x update-stack.sh
+
+
+
+log_detail "Validating swarm network infrastructure"
+NET_ID=$(docker network ls -f name=admin_network -q)
+
+if [[ ! -z "$NET_ID" ]]; then
+    log_detail "Removing network 'admin_network'"
+    docker network rm admin_network
+
+    log_detail "Waiting 5 seconds for network to be removed"
+    sleep 5
+fi
--- deploy.sh
+++ deploy.sh
@@ -5,50 +5,33 @@ set -e
 
 source ./scripts/consul.env
 source ./scripts/common_functions.sh
+cmdname=$(basename $0)
 
 log "*** >=>=>=>  Stack Deployment  <=<=<=< ***"
 
-log_detail "Swarm Details"
-
 export NUM_OF_MGR_NODES=$(docker info --format "{{.Swarm.Managers}}")
-log "Number of Manager Nodes: ${NUM_OF_MGR_NODES}"
-
 export NODE_IP=$(docker info --format "{{.Swarm.NodeAddr}}")
-log "Node IP: ${NODE_IP}"
-
 export NODE_ID=$(docker info --format "{{.Swarm.NodeID}}")
-log "Node ID: ${NODE_ID}"
-
 export NODE_NAME=$(docker info --format "{{.Name}}")
-log "Node Name: ${NODE_NAME}"
-
 export NODE_IS_MANAGER=$(docker info --format "{{.Swarm.ControlAvailable}}")
-log "Node Is Manager: ${NODE_IS_MANAGER}"
+show_node_details
 
-log_detail "Removing the following stacks: logging, ${CONSUL_STACK_PROJECT_NAME}"
 set +e
+log_detail "Removing the following stacks: logging, ${CONSUL_STACK_PROJECT_NAME}"
 docker stack rm "${CONSUL_STACK_PROJECT_NAME}"
-#logging
 
-log_detail "Removing the following services: devops_proxy"
-#docker service rm devops_proxy
+log_detail "Removing the following volumes: consul_data_volume"
+docker rm consul_data_volume
+
+log_detail "Removing network 'admin_network'"
+docker network rm admin_network
 set -e
 
-log_detail "Validating swarm network infrastructure"
-NET_ID=$(docker network ls -f name=admin_network -q)
-
-if [[ ! -z "$NET_ID" ]]; then
-    log_detail "Removing network 'admin_network'"
-    docker network rm admin_network
-
-    log_detail "Waiting 5 seconds for network to be removed"
-    sleep 5
-fi
+log_detail "Waiting 10 seconds for item deletion finalizes"
+sleep 10
 
 log_detail "Creating attachable overlay network 'admin_network'"
-set +e
 docker network create --driver=overlay --attachable --subnet=${CONSUL_SUBNET} admin_network
-set -e
 
 log_detail "Deploying DevOps Stack to Swarm"
 docker stack deploy --compose-file=./devops-stack.yml devops
