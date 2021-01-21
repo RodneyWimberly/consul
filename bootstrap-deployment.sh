1d71e482f0211d1343410a8c8006c23afb6bc883
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-21 11:19:43 -0800

    gfdsgdf

--- bootstrap-deployment.sh
+++ bootstrap-deployment.sh
@@ -32,6 +32,9 @@ rm -rf /tmp/consul
 git clone -b DeploymentTest \
     https://rodneywimberly:b606a0781f57605d4e5b00b753a6f26c23ff8908@github.com/RodneyWimberly/consul.git \
     /tmp/consul
+cd /tmp/consul
+chmod u+x *.sh
+chmod u+x ./scripts/*.sh
 exit
 EOL
 chmod u+x update-stack.sh
