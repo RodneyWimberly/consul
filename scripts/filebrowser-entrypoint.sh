92f6f1c5476ba02a7ce845a5ace8296d311addea
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-22 20:38:17 -0800

    kjjklfdzjklfd

--- scripts/filebrowser-entrypoint.sh
+++ scripts/filebrowser-entrypoint.sh
@@ -3,11 +3,13 @@
 source "${CONSUL_SCRIPT_DIR}"/consul.env
 source "${CONSUL_SCRIPT_DIR}"/common_functions.sh
 
+log "Installing Samba Server and OpenRC"
 # Setup samba server
 apk add samba openrc
 mkdir /svr
 chmod 0777 /srv
 
+log_detail "Configuring Samba Server"
 /etc/samba/smb.conf <<EOL
 [global]
     workgroup = WORKGROUP
@@ -21,13 +23,17 @@ chmod 0777 /srv
     path = /svr
 EOL
 
+log_detail "Creating user for Samba Server Service"
 echo "password" | adduser samba_user
 echo "password" | smbpasswd -a samba_user
 
+log_detail "Setting up the Samba Server Service"
 rc-update add samba
 rc-service samba start
 
+log_detail "Updating the user name and password for the file browser web site"
 filebrowser -d /root/.config/Filebrowser/filebrowser.db users update admin -p password
 echo ls -r /home
 
+log_detail "Starting the file browser web site"
 exec /filebrowser
