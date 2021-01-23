442aa0287d15463e367e2fb6225ab35db9c74b27
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-23 11:42:11 -0800

    xlg;;f

--- scripts/common_functions.sh
+++ scripts/common_functions.sh
@@ -44,6 +44,12 @@ function log_debug() {
   fi
 }
 
+function log_json() {
+  if [ ! -z "$CONSUL_DEBUG_LOG" ] && [ "$CONSUL_DEBUG_LOG" -ne "0" ] ; then
+    pretty_json=$(echo "$2" | jq)
+    log_raw "[DBG] $1": "$pretty_json"
+  fi
+}
 function log_raw() {
   echo "$(timestamp): $1"
 }
@@ -58,13 +64,13 @@ function append_generated_config() {
     generated_json=$(cat ${CONSUL_BOOTSTRAP_DIR}/generated.json)
     rm -f ${CONSUL_BOOTSTRAP_DIR}/generated.json
   fi
-  log_debug "Generated: ${generated_json}"
+  log_json "Generated" "${generated_json}"
 
   config_json=$(cat ${CONSUL_BOOTSTRAP_DIR}/${1})
-  log_debug "Config: ${config_json}"
+  log_json "Config" "${config_json}"
 
   generated_json=$(echo "${generated_json}" | jq ". + ${config_json}")
-  log_debug "Generated: ${generated_json}"
+  log_json "Generated" "${generated_json}"
 
   log_detail "Adding ${1} to generated.json"
   echo "${generated_json}" | jq ". + ${config_json}" > ${CONSUL_BOOTSTRAP_DIR}/generated.json
@@ -76,13 +82,13 @@ function expand_config_file_from() {
   log "Processing ${CONSUL_BOOTSTRAP_DIR}/$1 with variable expansion to ${CONSUL_CONFIG_DIR}/$1"
   rm -f "${CONSUL_CONFIG_DIR}/$1"
   cat "${CONSUL_BOOTSTRAP_DIR}/$1" | envsubst > "${CONSUL_CONFIG_DIR}/$1"
-  log_debug $(cat "${CONSUL_CONFIG_DIR}/$1")
+  log_json "$1" $(cat "${CONSUL_CONFIG_DIR}/$1")
   set -e
 }
 
 function get_node_details() {
   NODE_INFO=$(curl -sS --unix-socket /var/run/docker.sock http://localhost/info)
-  log_debug "Node Info: ${NODE_INFO}"
+  log_json "Node Info" "${NODE_INFO}"
   export NUM_OF_MGR_NODES=$(echo ${NODE_INFO} | jq -r -M '.Swarm.Managers')
   export NODE_IP=$(echo ${NODE_INFO} | jq -r -M '.Swarm.NodeAddr')
   export NODE_ID=$(echo ${NODE_INFO} | jq -r -M '.Swarm.NodeID')
@@ -103,11 +109,19 @@ function wait_for_bootstrap_process() {
   if [ -z "$CONSUL_HTTP_TOKEN" ] || [ "$CONSUL_HTTP_TOKEN" -eq "0" ] ; then
     log_detail 'Waiting 60 seconds before inquiring if the Consul cluster bootstrapping service to be complete'
     sleep 60
+
+    rest_response=$(curl -sS --connect-timeout 180 --unix-socket /var/run/docker.sock -X POST http://localhost/containers/json)
+    log_json "Container List" "${rest_response}"
+
+    rest_response=$(curl -sS --connect-timeout 180 --unix-socket /var/run/docker.sock -X POST http://localhost/containers/json?filters={"label": ["name=consul-bootstrapper"]})
+    log_json "Container List Filter" "${rest_response}"
+
     log_detail "Querying Docker REST API to see if service ${CONSUL_STACK_PROJECT_NAME}_consul-bootstrapper has completed"
     rest_response=$(curl -sS --connect-timeout 180 --unix-socket /var/run/docker.sock -X POST http://localhost/containers/${CONSUL_STACK_PROJECT_NAME}_consul-bootstrapper/wait)
-    log_debug "REST Response: ${rest_response}"
+    log_json "REST Response" "${rest_response}"
+
     status_code=$(echo ${rest_response} | jq -r -M '.StatusCode')
-    if [ status_code -eq 0 ]; then
+    if [ "$status_code" -eq "0" ]; then
       log_detail "The consul cluster has been successfully bootstrapped."
     else
       error_msg=$(echo ${rest_response} | jq -r -M '.Error.Message')
@@ -118,7 +132,7 @@ function wait_for_bootstrap_process() {
     fi
   else
     log_detail "The master ACL Token is present so skipping the bootstrap process."
-    log_debug "CONSUL_HTTP_TOKEN: ${CONSUL_HTTP_TOKEN}"
+    log_json "CONSUL_HTTP_TOKEN" "${CONSUL_HTTP_TOKEN}"
   fi
 }
 
