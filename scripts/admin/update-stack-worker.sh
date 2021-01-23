3960bdf754fb3fa35fb74206c831da5e7ebaa8a2
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-23 08:50:10 -0800

    sfgd

--- scripts/admin/update-stack-worker.sh
+++ scripts/admin/update-stack-worker.sh
@@ -8,8 +8,8 @@ rm -rf /tmp/consul
 git clone -b DeploymentTest \
     https://rodneywimberly:b606a0781f57605d4e5b00b753a6f26c23ff8908@github.com/RodneyWimberly/consul.git \
     /tmp/consul
-cd /tmp/consul
-chmod u+x ./scripts/*.sh
+cd /tmp/consul/scripts
+chmod u+x ./*.sh
 exit
 EOL
 chmod u+x update-stack.sh
