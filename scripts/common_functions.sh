15a9dad7adab881e17e2ca5f3089cf18e217e743
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-23 04:30:28 -0800

    sdfgdf

--- scripts/common_functions.sh
+++ scripts/common_functions.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-source "${CONSUL_SCRIPT_DIR}"/consul.env
+source consul.env
 
 function consul_cmd() (
   consul_container="$(docker stack ps -q -f name=${CONSUL_STACK_PROJECT_NAME}_consul ${CONSUL_STACK_PROJECT_NAME})"
