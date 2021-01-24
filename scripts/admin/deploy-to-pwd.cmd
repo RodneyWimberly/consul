dc2b3cc2b7229cc3ba0a1b24c8d195561e6a5e14
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-24 15:42:53 -0800

    sdcvd

--- scripts/admin/deploy-to-pwd.cmd
+++ scripts/admin/deploy-to-pwd.cmd
@@ -17,7 +17,7 @@ rem git pull --force --progress https://rodneywimberly:b606a0781f57605d4e5b00b75
 
 rem ==================================================
 echo " --> Executing update-stack.sh on Worker1"
-start "Worker1 Update" /B putty.exe -m d:\consul\soakes\consul\scripts\admin\pwd-remote-cmd.sh ip172-18-0-7-c06icur6hnp000d51o6g@direct.labs.play-with-docker.com
+start "Worker1 Update" /B putty.exe -m d:\consul\soakes\consul\scripts\admin\pwd-remote-cmd.sh ip172-18-0-7-c0703eb6hnp000fnpnq0@direct.labs.play-with-docker.com
 
 echo " --> Executing update-stack.sh on Manager1"
-start "Manager1 Update" /B putty.exe -m  d:\consul\soakes\consul\scripts\admin\pwd-remote-cmd.sh ip172-18-0-65-c06icur6hnp000d51o6g@direct.labs.play-with-docker.com
+start "Manager1 Update" /B putty.exe -m  d:\consul\soakes\consul\scripts\admin\pwd-remote-cmd.sh ip172-18-0-38-c0703eb6hnp000fnpnq0@direct.labs.play-with-docker.com
