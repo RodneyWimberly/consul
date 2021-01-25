39e8d0cbe21a3cb72755444a59714ac5c74ef014
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-25 10:06:25 -0800

    dasfdf

--- scripts/admin/deploy-to-pwd.cmd
+++ scripts/admin/deploy-to-pwd.cmd
@@ -1,10 +1,14 @@
+
 @echo off
 cls
 
 rem ==================================================
 SET SCRIPT_PATH=d:\consul\soakes\consul\scripts\admin
 SET DOCKER_HUB=docker.pkg.github.com
-SET DOCKER_REGISTRY=ip172-18-0-46-c074tpk34gag00brhs4g-5000.direct.labs.play-with-docker.com/
+rem SET DOCKER_REGISTRY=localhost:5000/
+rem SET DOCKER_REGISTRY=ip172-18-0-46-c074tpk34gag00brhs4g-5000.direct.labs.play-with-docker.com/
+rem SET DOCKER_REGISTRY=docker.pkg.github.com/rodneywimberly/dockerrepositories/
+SET DOCKER_REGISTRY=rodneywimberly/dockerregistry:
 SET REMOTE_CMD=d:\consul\soakes\consul\scripts\admin\pwd-remote-cmd.sh
 SET PWD_URL=direct.labs.play-with-docker.com
 SET WORKER=ip172-18-0-44-c074tpk34gag00brhs4g@%PWD_URL%
@@ -16,13 +20,14 @@ cd %SCRIPT_PATH%
 rem ==================================================
 echo "Build and Deploy Docker Images to Docker Hub"
 echo " --> Building consul-bootstrapper image for consul stack"
-docker build -t %DOCKER_REGISTRY%consul-bootstrapper ../../bootstrapper/.
+docker build -t %DOCKER_REGISTRY%consul-bootstrapper:1.0 ../../bootstrapper/.
 
 echo " --> Logging in to repository %DOCKER_REGISTRY%"
 rem docker login https://%DOCKER_HUB% --username=RodneyWimberly --password=5a45a7688ea36d4572100a47f894435fef6b2aa5
+docker login --username=rodneywimberly --password=P@55w0rd!
 
 echo " --> Pushing consul-bootstrapper image for consul stack"
-docker push %DOCKER_REGISTRY%consul-bootstrapper
+docker push %DOCKER_REGISTRY%consul-bootstrapper:1.0
 
 rem ==================================================
 rem "Tell Play with Docker Lab to Get Latest and Deploy"
