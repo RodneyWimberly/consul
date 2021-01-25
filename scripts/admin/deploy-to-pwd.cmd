72e8d2c7023986ee311f4b2b1789a361ba47d9f0
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-24 18:14:29 -0800

    FIxed Dockerfile issue

--- scripts/admin/deploy-to-pwd.cmd
+++ scripts/admin/deploy-to-pwd.cmd
@@ -1,23 +1,26 @@
 @echo off
 cls
-cd d:\consul\soakes\consul
+cd d:\consul\soakes\consul\scripts\admin
 
 rem ==================================================
-rem echo " --> Adding changed items to source control"
-rem git add .
+SET DOCKER_REGISTRY=docker.pkg.github.com/rodneywimberly/dockerrepositories/
+SET REMOTE_CMD=d:\consul\soakes\consul\scripts\admin\pwd-remote-cmd.sh
+SET WORKER=ip172-18-0-7-c0703eb6hnp000fnpnq0@direct.labs.play-with-docker.com
+SET MANAGER=ip172-18-0-38-c0703eb6hnp000fnpnq0@direct.labs.play-with-docker.com
 
-rem echo " --> Commiting changed items to source control"
-rem git commit -m "<your message here>"
+rem ==================================================
+echo "Building consul-bootstrapper image for consul stack"
+docker build -t %DOCKER_REGISTRY%consul-bootstrapper:1.0 bootstrapper/.
 
-rem echo " --> Pushing changed items to source control"
-rem git push --progress --all --repo=https://rodneywimberly:b606a0781f57605d4e5b00b753a6f26c23ff8908@github.com/RodneyWimberly/consul.git
+echo "Logging in to repository ${DOCKER_REGISTRY}"
+docker login https://docker.pkg.github.com--username=RodneyWimberly --password=5a45a7688ea36d4572100a47f894435fef6b2aa5
 
-rem echo " --> Pulling changed items from source control"
-rem git pull --force --progress https://rodneywimberly:b606a0781f57605d4e5b00b753a6f26c23ff8908@github.com/RodneyWimberly/consul.git
+echo "Pushing consul-bootstrapper image for consul stack"
+docker push %DOCKER_REGISTRY%consul-bootstrapper:1.0
 
 rem ==================================================
 echo " --> Executing update-stack.sh on Worker1"
-start "Worker1 Update" /B putty.exe -m d:\consul\soakes\consul\scripts\admin\pwd-remote-cmd.sh ip172-18-0-7-c0703eb6hnp000fnpnq0@direct.labs.play-with-docker.com
+start "Worker Node Update" /B putty.exe -m %REMOTE_CMD% %WORKER%
 
 echo " --> Executing update-stack.sh on Manager1"
-start "Manager1 Update" /B putty.exe -m  d:\consul\soakes\consul\scripts\admin\pwd-remote-cmd.sh ip172-18-0-38-c0703eb6hnp000fnpnq0@direct.labs.play-with-docker.com
+start "Manager Node Update" /B putty.exe -m %REMOTE_CMD% %MANAGER%
