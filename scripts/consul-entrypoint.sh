dace72351bdc54ef6228d66bdd9ea9e9124790ba
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-24 20:49:10 -0800

    kljlkj

--- scripts/consul-entrypoint.sh
+++ scripts/consul-entrypoint.sh
@@ -8,7 +8,12 @@ apk add bash curl jq gettext
 
 add_path "${CONSUL_SCRIPT_DIR}"
 
-get_docker_details
+NODE_INFO=$(docker_api "info")
+export NUM_OF_MGR_NODES=$(echo ${NODE_INFO} | jq -r -M '.Swarm.Managers')
+export NODE_IP=$(echo ${NODE_INFO} | jq -r -M '.Swarm.NodeAddr')
+export NODE_ID=$(echo ${NODE_INFO} | jq -r -M '.Swarm.NodeID')
+export NODE_NAME=$(echo ${NODE_INFO} | jq -r -M '.Name')
+export NODE_IS_MANAGER=$(echo ${NODE_INFO} | jq -r -M '.Swarm.ControlAvailable')
 
 expand_config_file_from "common.json"
 if [ "${NODE_IS_MANAGER}" == "true" ]; then
@@ -19,9 +24,25 @@ else
   expand_config_file_from "client.json"
 fi
 
-wait_for_bootstrap_process
+if [ -z "$CONSUL_HTTP_TOKEN" ] || [ "$CONSUL_HTTP_TOKEN" -eq "0" ] ; then
+    log 'Waiting before inquiring if the Consul cluster bootstrapping service to be complete'
+    ${CONSUL_SCRIPT_DIR}/wait-for-it.sh --timeout=300 --host=consul-bootstrapper.service.consul --port=80 --strict -- echo "consul-bootstrapper found" || (echo "Failed to locate consul-bootstrapper" && exit 1)
 
-show_docker_details
+    sleep 5
+    log_detail "Querying the bootstrap process to see if it has completed."
+    curl http://consul-bootstrapper.service.consul/${agent_mode}.json -o ${CONSUL_CONFIG_DIR}/${agent_mode}.json
+
+    log_detail "The consul cluster has been successfully bootstrapped."
+else
+    log_detail "The master ACL Token is present so skipping the bootstrap process."
+fi
+
+log ">=>=>=>=>=>  Swarm/Node Details  <=<=<=<=<=<"
+log "Number of Manager Nodes: ${NUM_OF_MGR_NODES}"
+log "Node IP: ${NODE_IP}"
+log "Node ID: ${NODE_ID}"
+log "Node Name: ${NODE_NAME}"
+log "Node Is Manager: ${NODE_IS_MANAGER}"
 
 log_detail "Starting Consul in ${agent_mode} mode using the following command: exec docker-entrypoint.sh $@"
 exec docker-entrypoint.sh "$@"
