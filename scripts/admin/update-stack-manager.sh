0e71c024d48c05aa2814f55d9b605000d7ae5e04
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-25 16:23:39 -0800

    fg

--- scripts/admin/update-stack-manager.sh
+++ scripts/admin/update-stack-manager.sh
@@ -8,10 +8,18 @@ rm -rf /tmp/consul
 git clone -b DeploymentTest \
      https://rodneywimberly:b606a0781f57605d4e5b00b753a6f26c23ff8908@github.com/RodneyWimberly/consul.git \
     /tmp/consul
+cp -r /tmp/consul/scripts/* /mnt/scripts/
+cp -r /tmp/consul/backups/* /mnt/backups/
+cp -r /tmp/consul/certs/* /mnt/certs/
+cp -r /tmp/consul/config/* /mnt/config/
 cd /tmp/consul/scripts
 chmod u+x ./*.sh
 ./deploy.sh
 exit
 EOL
 chmod u+x update-stack.sh
+mkdir -p /mnt/backups
+mkdir -p /mnt/config
+mkdir -p /mnt/certs
+mkdir -p /mnt/scripts
 ./update-stack.sh
