9b44bb35c59568c4fab75c7c99eaed310fb97818
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-25 23:18:44 -0800

    sdf

--- scripts/admin/deploy-to-pwd.cmd
+++ scripts/admin/deploy-to-pwd.cmd
@@ -10,9 +10,9 @@ rem SET DOCKER_REGISTRY=docker.pkg.github.com/rodneywimberly/dockerrepositories/
 SET DOCKER_REGISTRY=rodneywimberly/
 SET REMOTE_CMD=d:\projects\consul\scripts\admin\pwd-remote-cmd.sh
 SET PWD_URL=direct.labs.play-with-docker.com
-SET WORKER=ip172-18-0-27-c07loor6hnp000dtdnig@%PWD_URL%
+SET WORKER=ip172-18-0-42-c07r5kc34gag00b7u0pg@%PWD_URL%
 SET P1=https://labs.play-with-docker.com/p/c074tpk34gag00brhs4g#c074tpk3_c074trc34gag00brhs50
-SET MANAGER=ip172-18-0-44-c07loor6hnp000dtdnig@%PWD_URL%
+SET MANAGER=ip172-18-0-44-c07r5kc34gag00b7u0pg@%PWD_URL%
 SET M1=https://labs.play-with-docker.com/p/c074tpk34gag00brhs4g#c074tpk3_c074trc34gag00brhs5g
 cd %SCRIPT_PATH%
 
