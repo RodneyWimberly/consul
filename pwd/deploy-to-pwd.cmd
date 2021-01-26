dbd9b686ffab2a03e51e35b05af885481801be30
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-26 13:02:44 -0800

    fgdsf

--- pwd/deploy-to-pwd.cmd
+++ pwd/deploy-to-pwd.cmd
@@ -4,14 +4,14 @@ rem ==================================================
 SET SCRIPT_PATH=d:\projects\consul\pwd
 SET REMOTE_CMD=d:\projects\consul\pwd\pwd-remote-cmd.sh
 SET PWD_URL=direct.labs.play-with-docker.com
-SET WORKER=ip172-18-0-42-c07r5kc34gag00b7u0pg@%PWD_URL%
-SET MANAGER=ip172-18-0-44-c07r5kc34gag00b7u0pg@%PWD_URL%
+SET WORKER=ip172-18-0-84-c0885ss34gag00b7v180
+SET MANAGER=ip172-18-0-89-c0885ss34gag00b7v180
 cd %SCRIPT_PATH%
 
 rem ==================================================
 rem "Tell Play with Docker Lab to Get Latest and Deploy"
 echo " --> Executing update-stack.sh on Worker1"
-start "Worker Node Update" /B putty.exe -m %REMOTE_CMD% %WORKER%
+start "Worker Node Update" /B putty.exe -m %REMOTE_CMD% %WORKER%@%PWD_URL%
 
 echo " --> Executing update-stack.sh on Manager1"
-start "Manager Node Update" /B putty.exe -m %REMOTE_CMD% %MANAGER%
+start "Manager Node Update" /B putty.exe -m %REMOTE_CMD% %MANAGER%@%PWD_URL%
