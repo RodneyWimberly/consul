a803d46a0705e5614786ab4be6728b05486827ea
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-26 21:46:25 -0800

    New PWD Deployment Added Portianer local data mount instead of docker volume

--- pwd/set-variables.cmd
+++ pwd/set-variables.cmd
@@ -1,6 +1,6 @@
 SET SCRIPT_PATH=d:\projects\consul\pwd
 SET REMOTE_CMD=d:\projects\consul\pwd\pwd-remote-cmd.sh
 SET PWD_URL=direct.labs.play-with-docker.com
-SET WORKER=ip172-18-0-32-c08aoqr6hnp000cuqk2g
-SET MANAGER=ip172-18-0-34-c08aoqr6hnp000cuqk2g
+SET WORKER=ip172-18-0-56-c08fnt1lo55000ehlpgg
+SET MANAGER=ip172-18-0-57-c08fnt1lo55000ehlpgg
 cd %SCRIPT_PATH%
