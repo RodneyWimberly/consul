c7ad132b7ca482d6870dea9bb7ad4599fa40808d
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-27 14:22:40 -0800

    Lots

--- scripts/jump-box-entrypoint.sh
+++ scripts/jump-box-entrypoint.sh
@@ -20,9 +20,14 @@ apk add curl jq openssl gettext iputils nfs-utils iproute2 sudo
 sudo -u root mount -v -o vers=4,loud nfsserver_storage:/ /mnt/svc
 sudo -u root mount -v -o vers=4,loud 192.168.100.100:/ /mnt/ip
 sudo -u root mount -v -o vers=4,loud storage.service.consul:/ /mnt/fqdn
+sudo -u root mount -v -o vers=4,loud tasks.nfsserver_storage:/ /mnt/fqdn
+sudo -u root mount -v -o vers=4,loud storage:/ /mnt/fqdn
 df -h
 
-while [[ "true" == "true" ]]; do
-    log "Sleeping 300 seconds so container task won't complete"
-    sleep 300
-done
+exec sh -c 'while true ;do wait ;done'
+#while true; do :; done & kill -STOP $! && wait $!
+#tail -f /dev/null
+# while [[ "true" == "true" ]]; do
+#     log "Sleeping 300 seconds so container task won't complete"
+#     sleep 300
+# done
