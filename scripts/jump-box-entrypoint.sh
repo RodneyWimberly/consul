a4d2e3863c2db907055455ac5932476db4d052c4
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-27 00:01:17 -0800

    knmknkl

--- scripts/jump-box-entrypoint.sh
+++ scripts/jump-box-entrypoint.sh
@@ -16,8 +16,11 @@ apk add \
   iproute2 \
   openssl
 
-mkdir /mnt
-mount -v -o vers=4,loud 192.168.100.4:/ /mnt
+apk add curl jq openssl gettext iputils nfs-utils iproute2 sudo
+sudo -u root mount -v -o vers=4,loud nfsserver_storage:/ /mnt/svc
+sudo -u root mount -v -o vers=4,loud 192.168.100.100:/ /mnt/ip
+sudo -u root mount -v -o vers=4,loud storage.service.consul:/ /mnt/fqdn
+df -h
 
 while [[ "true" == "true" ]]; do
     log "Sleeping 300 seconds so container task won't complete"
