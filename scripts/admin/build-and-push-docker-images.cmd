7d8d90d0da40c6152f757bd9e5901453e55afbfa
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-26 16:48:58 -0800

    df

--- scripts/admin/build-and-push-docker-images.cmd
+++ scripts/admin/build-and-push-docker-images.cmd
@@ -2,12 +2,12 @@
 
 rem ==================================================
 SET SCRIPT_PATH=d:\projects\consul\scripts\admin
-SET DOCKER_HUB=docker.pkg.github.com/
-rem SET DOCKER_HUB=docker.io/
+rem SET DOCKER_HUB=docker.pkg.github.com/
+SET DOCKER_HUB=docker.io/
 rem SET DOCKER_REGISTRY=localhost:5000/
 rem SET DOCKER_REGISTRY=ip172-18-0-46-c074tpk34gag00brhs4g-5000.direct.labs.play-with-docker.com/
-SET DOCKER_REGISTRY=rodneywimberly/consul/
-rem SET DOCKER_REGISTRY=rodneywimberly/
+rem SET DOCKER_REGISTRY=rodneywimberly/consul/
+SET DOCKER_REGISTRY=rodneywimberly/
 SET DOCKER_TAG=:1.0
 SET DOCKER_FULL_URL=docker.pkg.github.com/rodneywimberly/dockerrepositories/$image$:1.0
 cd %SCRIPT_PATH%
@@ -15,17 +15,17 @@ cd %SCRIPT_PATH%
 rem ==================================================
 echo "Build and Deploy Docker Images to Docker Hub"
 echo " --> Building consul-bootstrapper image for consul stack"
-docker build -t %DOCKER_HUB%%DOCKER_REGISTRY%bootstrapper%DOCKER_TAG% ../../bootstrapper/.
+docker build -t %DOCKER_HUB%%DOCKER_REGISTRY%dockerrepositories:bootstrapper ../../bootstrapper/.
 
 echo " --> Building volume image for nfstest stack"
-docker build -t %DOCKER_HUB%%DOCKER_REGISTRY%nfstest%DOCKER_TAG% ../../nfs/.
+docker build -t %DOCKER_HUB%%DOCKER_REGISTRY%dockerrepositories:nfstest ../../nfs/.
 
 echo " --> Logging in to repository %DOCKER_REmGISTRY%"
-docker login https://%DOCKER_HUB% --username=RodneyWimberly --password=b1b203616d5b8f247d0a0749ebc02ecdac81a7d3
-rem docker login --username=rodneywimberly --password=P@55w0rd!
+rem docker login https://%DOCKER_HUB% --username=RodneyWimberly --password=b1b203616d5b8f247d0a0749ebc02ecdac81a7d3
+docker login --username=rodneywimberly --password=P@55w0rd!
 
 echo " --> Pushing consul-bootstrapper image for consul stack"
-docker push %DOCKER_HUB%%DOCKER_REGISTRY%bootstrapper%DOCKER_TAG%
+docker push %DOCKER_HUB%%DOCKER_REGISTRY%dockerrepositories:bootstrapper
 
 echo " --> Pushing volume image for nfstest stack"
-docker push %DOCKER_HUB%%DOCKER_REGISTRY%nfstest%DOCKER_TAG%
+docker push %DOCKER_HUB%%DOCKER_REGISTRY%dockerrepositories:nfstest
