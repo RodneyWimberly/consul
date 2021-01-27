c23a68d1e3b7bc28e0647bd683e6cd5b55fc7548
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-26 22:53:02 -0800

    ???

--- pwd/set-variables.cmd
+++ pwd/set-variables.cmd
@@ -1,6 +1,6 @@
 SET SCRIPT_PATH=d:\projects\consul\pwd
 SET REMOTE_CMD=d:\projects\consul\pwd\pwd-remote-cmd.sh
 SET PWD_URL=direct.labs.play-with-docker.com
-SET WORKER=ip172-18-0-56-c08fnt1lo55000ehlpgg
-SET MANAGER=ip172-18-0-57-c08fnt1lo55000ehlpgg
+SET WORKER=ip172-18-0-65-c08fnt1lo55000ehlpgg
+SET MANAGER=ip172-18-0-66-c08fnt1lo55000ehlpgg
 cd %SCRIPT_PATH%
