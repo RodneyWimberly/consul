7e50d626f5de3ac6f9f0391c75cf642e4ebca51d
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-28 02:52:58 -0800

    njkl

--- pwd/connect-to-pwd.ps1
+++ pwd/connect-to-pwd.ps1
@@ -1,13 +1,14 @@
 Clear-Host
 .\set-pwd-variables.ps1
 
-Push-Location $Env:ScriptPath
-Start-Ssh --Node $Env:Worker
-Start-Ssh --Node $Env:Manager
-Pop-Location
 
 function Start-Ssh {
     param($Node = "")
-    Write-Verbose " --> Opening ssh to " + $Env:PwdUrl + " for Node " + $Node
-    Start-Process --FilePath "putty.exe" --ArgumentList [ $Node + "@" + $Env:PwdUrl ]
+    Write-Host " --> Opening ssh to " & $Env:PwdUrl & " for Node " & $Node
+    Start-Process --FilePath "putty.exe" --ArgumentList  $Node & "@" & $Env:PwdUrl
 }
+
+Push-Location $Env:ScriptPath
+Start-Ssh $Env:Worker
+Start-Ssh $Env:Manager
+Pop-Location
--- pwd/set-pwd-variables.ps1
+++ pwd/set-pwd-variables.ps1
@@ -1,6 +1,6 @@
 $Env:ScriptPath="d:\projects\consul\pwd"
 $Env:RemoteCommand="d:\projects\consul\pwd\pwd-remote-cmd.sh"
 $Env:PwdUrl="direct.labs.play-with-docker.com"
-$Env:Worker="ip172-18-0-38-c08tn836hnp000esrq6g"
-$Env:Manager="ip172-18-0-67-c08tn836hnp000esrq6g"
+$Env:Worker="ip172-18-0-26-c0997n1lo55000ab48b0"
+$Env:Manager="ip172-18-0-21-c0997n1lo55000ab48b0"
 
