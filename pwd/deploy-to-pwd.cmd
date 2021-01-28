0e53030e73f6b107b9a489b2b07f0e45a8be754a
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-28 01:48:20 -0800

    Switch local scripts to PowerShell Updated startup to look for local snapshots of Consul cluster Updated Consul ACL configuration with bootstrap values. Hopefully new deployments can use this as a starting base.

--- pwd/deploy-to-pwd.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-cls
-call set-variables.cmd
-
-echo " --> Executing update-stack.sh on Worker1"
-start "Worker Node Update" /B putty.exe -m %REMOTE_CMD% %WORKER%@%PWD_URL%
-
-echo " --> Executing update-stack.sh on Manager1"
-start "Manager Node Update" /B putty.exe -m %REMOTE_CMD% %MANAGER%@%PWD_URL%
---
+++ pwd/set-pwd-variables.ps1
@@ -0,0 +1,6 @@
+$Env:ScriptPath="d:\projects\consul\pwd"
+$Env:RemoteCommand="d:\projects\consul\pwd\pwd-remote-cmd.sh"
+$Env:PwdUrl="direct.labs.play-with-docker.com"
+$Env:Worker="ip172-18-0-38-c08tn836hnp000esrq6g"
+$Env:Manager="ip172-18-0-67-c08tn836hnp000esrq6g"
+
