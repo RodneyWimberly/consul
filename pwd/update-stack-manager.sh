a309a6346cb46d76236beb57698d4b6fcff1abcb
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-28 13:57:52 -0800

    knkljnkl

--- pwd/update-stack-manager.sh
+++ pwd/update-stack-manager.sh
@@ -23,6 +23,6 @@ mkdir -p /mnt/certs
 mkdir -p /mnt/scripts
 mkdir -p /mnt/webmgr
 apk add screen git
-echo "caption always \"%{= kc}Screen %S on %H -20=%{= .m}%D %d.%m.%Y %0c\"" > ~/.screenrc
+echo "caption always \"%{= kc}Screen %S on %H %-20=%{= .m}%D %d.%m.%Y %0c\"" > ~/.screenrc
 screen -q -t update-stack -S update-stack
 ./update-stack.sh
