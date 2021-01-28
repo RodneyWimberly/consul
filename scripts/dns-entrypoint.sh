f3d9008db34cebfb4955dca3012ec8349f82f39c
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-28 15:41:54 -0800

    Added Dns

---
+++ scripts/dns-entrypoint.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# Make our stuff available
+source "${CONSUL_SCRIPT_DIR}"/consul.env
+source "${CONSUL_SCRIPT_DIR}"/common_functions.sh
+add_path "${CONSUL_SCRIPT_DIR}"
+
+# Update existing packages
+apk update
+
+# Add required packages
+apk add \
+  curl \
+  jq \
+  gettext \
+  openssl
+
+# Add optional packages
+apk add \
+  bash \
+  sudo \
+  git \
+  screen \
+  iputils \
+  iproute2 \
+  nfs-utils
+
+# Get Docker/Node/Hosting information from the Docker API for use in configuration
+NODE_INFO=$(docker_api "info")
+export NUM_OF_MGR_NODES=$(echo ${NODE_INFO} | jq -r -M '.Swarm.Managers')
+export NODE_IP=$(echo ${NODE_INFO} | jq -r -M '.Swarm.NodeAddr')
+export NODE_ID=$(echo ${NODE_INFO} | jq -r -M '.Swarm.NodeID')
+export NODE_NAME=$(echo ${NODE_INFO} | jq -r -M '.Name')
+export NODE_IS_MANAGER=$(echo ${NODE_INFO} | jq -r -M '.Swarm.ControlAvailable')
+show_docker_details
+
+set +e
+log "Looking up the IP address for Consul to set as Consul domain owner"
+while true; do
+  log_detail "waiting 10 seconds for Consul to come up and respond on the IP layer"
+  sleep 10
+
+  log_detail "querying for service tasks.consul_consul"
+  CONSUL_IP="`dig +short tasks.consul_consul | tail -n1`"
+
+  log_detail "merging expanded variables with configuration templates and placeing in the config folder"
+  cat /etc/dnsmasq.template | envsubst > /etc/dnsmasq/dnsmasq.conf
+
+  log_detail "Starting DnsMasq"
+  dnsmasq --no-daemon --log-queries --server=/consul/"$${CONSUL_IP}"#8600
+done
