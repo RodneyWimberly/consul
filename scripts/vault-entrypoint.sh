6162d5d536efe5e58522b3402a047dae3d3d2f19
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-30 03:23:40 -0800

    sndfzm,.snx

--- scripts/vault-entrypoint.sh
+++ scripts/vault-entrypoint.sh
@@ -13,12 +13,16 @@ set +e
 
 # Get Docker/Node/Hosting information from the Docker API for use in configuration
 hosting_details
-# log "-----------------------------------------------------------"
-# log "- Vault Details"
-# log "-----------------------------------------------------------"
-# log_detail "Vault API URL: ${VAULT_API_ADDR}"
-# log_detail "Vault Cluster URL: ${VAULT_CLUSTER_ADDR}"
-# log ""
+export VAULT_ADDR="http://${ETH0_IP}:8200"
+export VAULT_API_ADDR="http://${ETH0_IP}:8200"
+export VAULT_CLUSTER_ADDR="https://${ETH0_IP}:8201"
+log "-----------------------------------------------------------"
+log "- Vault Details"
+log "-----------------------------------------------------------"
+log_detail "Vault Address: ${VAULT_ADDR}"
+log_detail "Vault API Address: ${VAULT_API_ADDR}"
+log_detail "Vault Cluster Address: ${VAULT_CLUSTER_ADDR}"
+log ""
 
 log_detail "merging expanded variables with configuration templates and placing in the config folder"
 #cat /vault/templates/vault.json | envsubst > /vault/config/vault.json
