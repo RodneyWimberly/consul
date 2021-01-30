183f5b49e5b5fa80e4e5b8272727e6a8a39cea6f
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-29 16:18:45 -0800

    Added lots of stacks

---
+++ scripts/nginx-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# this command will automatically register the portal app as a consul service
+set -ex
+type curl || (
+  until apt-get update; do sleep 3; done
+  until apt-get -y --no-install-recommends install unzip curl ca-certificates procps less vim; do sleep 3; done
+)
+curl -fLo /usr/share/nginx/html/stylesheet.css https://raw.githubusercontent.com/samrocketman/jervis-api/gh-pages/1.6/stylesheet.css
+curl -fLo ~/.vimrc https://raw.githubusercontent.com/samrocketman/home/master/dotfiles/.vimrc
+consul-agent.sh --service '{"service": {"name": "portal", "tags": [], "port": 80}}' \
+  --consul-template-file-cmd /nginx.conf nginx.tpl /etc/nginx/conf.d/default.conf "consul lock -name service/portal -shell=false reload nginx -s reload" \
+  --consul-template-file /index.html index.html.tpl /usr/share/nginx/html/index.html
+exec nginx -g 'daemon off;'
