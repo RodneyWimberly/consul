18a264288653be8234cee174c1a025aa2fb59973
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-29 16:18:45 -0800

    Added lots of stacks

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,8 +16,8 @@ export NODE_IS_MANAGER=$(docker info --format "{{.Swarm.ControlAvailable}}")
 show_docker_details
 
 set +e
-log_detail "Removing the following stacks: ${CORE_STACK_NAME}"
-docker stack rm "${CORE_STACK_NAME}"
+log_detail "Removing the following stacks: ${CORE_STACK_NAME}, ${LOGGING_STACK_NAME}, and ${UI_STACK_NAME}"
+docker stack rm "${CORE_STACK_NAME}" ${LOGGING_STACK_NAME} ${UI_STACK_NAME}
 
 #log_detail "Removing the following volumes: consul_data_volume"
 #docker rm consul_data_volume
@@ -29,7 +29,12 @@ log_detail "Waiting 1 seconds for item deletion finalizes"
 sleep 1
 
 log_detail "Creating attachable overlay network 'admin_network'"
-docker network create --driver=overlay --attachable --subnet=${CORE_SUBNET} admin_network
+#docker network create --driver=overlay --attachable --subnet=${CORE_SUBNET} admin_network
+docker network create --driver=overlay --attachable admin_network
+
+log_detail "Creating attachable overlay network 'api_network'"
+#docker network create --driver=overlay --attachable --subnet=${CORE_SUBNET} admin_network
+docker network create --driver=overlay --attachable api_network
 set -e
 
 #log_detail "Logging into GitHub Registry"
@@ -38,17 +43,16 @@ set -e
 # log_detail "Deploying Storage Stack to Swarm"
 # docker stack deploy --compose-file=/tmp/consul/storage-stack.yml storage
 
-log_detail "Deploying DevOps Stack to Swarm"
-docker stack deploy --compose-file=/tmp/consul/devops-stack.yml devops
+log_detail "Deploying ${DEVOPS_STACK_NAME} Stack to Swarm"
+docker stack deploy --compose-file=/tmp/consul/"${DEVOPS_STACK_NAME}"-stack.yml "${DEVOPS_STACK_NAME}"
 
-#log_detail "Deploying Diagnostics Stack to Swarm"
-#docker stack deploy --compose-file=./diagnostics-stack.yml diagnostics
+log_detail "Deploying ${UI_STACK_NAME} Stack to Swarm"
+docker stack deploy --compose-file=./"${UI_STACK_NAME}"-stack.yml "${UI_STACK_NAME}"
 
-#log_detail "Deploying Logging Stack to Swarm"
-#docker stack deploy --compose-file=./logging-stack.yml logging
+log_detail "Deploying ${LOGGING_STACK_NAME} Stack to Swarm"
+docker stack deploy --compose-file=./"${LOGGING_STACK_NAME}"-stack.yml "${LOGGING_STACK_NAME}"
 
 log_detail "Deploying ${CORE_STACK_NAME} stack to swarm"
-#cat /tmp/consul/"${CORE_STACK_NAME}"-stack.yml | envsubst > /tmp/consul/deploy.yml
 docker stack deploy --compose-file=/tmp/consul/"${CORE_STACK_NAME}"-stack.yml "${CORE_STACK_NAME}"
 
 # log_detail "Deploying NfsClient Stack to Swarm"
