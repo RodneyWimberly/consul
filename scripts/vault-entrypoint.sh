a75d1e19880f090fb22bca1f5f722cf9d6521502
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-30 03:12:53 -0800

    l;lxl'm

--- scripts/vault-entrypoint.sh
+++ scripts/vault-entrypoint.sh
@@ -8,7 +8,7 @@ set +e
 #su -c "apk update"
 
 # Add required packages
-apk add --no-cache gettext
+#apk add --no-cache gettext
 # curl jq
 
 # Get Docker/Node/Hosting information from the Docker API for use in configuration
@@ -21,6 +21,27 @@ hosting_details
 # log ""
 
 log_detail "merging expanded variables with configuration templates and placing in the config folder"
-cat /vault/templates/vault.json | envsubst > /vault/config/vault.json
+#cat /vault/templates/vault.json | envsubst > /vault/config/vault.json
+
+cat > /vault/config/vault.json <<EOL
+{
+  "ui": true,
+  "backend": {
+    "consul": {
+      "address": "consul.service.consul:8500",
+      "path": "vault/",
+      "scheme": "http"
+    }
+  },
+  "default_lease_ttl": "168h",
+  "listener": {
+    "tcp": {
+      "address": "${ETHO_IP}:8200",
+      "tls_disable": "1"
+    }
+  },
+  "max_lease_ttl": "720h"
+}
+EOL
 
 docker-entrypoint.sh "$@"
