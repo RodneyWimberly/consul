4c541e8f1dcb760d5fa6cbdb81755870ed82f46f
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-29 19:30:30 -0800

    kldfsl;k

--- scripts/vault-entrypoint.sh
+++ scripts/vault-entrypoint.sh
@@ -9,23 +9,19 @@ apk update
 
 # Add required packages
 apk add --no-cache \
-  curl \
-  jq \
   iputils \
   iproute2 \
   bind-tools \
-  gettext \
-  openssl \
-  lshw
+  gettext
 
-# Get Docker/Node/Hosting information from the Docker API for use in configuration
-hosting_details
+set -elog_detail "merging expanded variables with configuration templates and placing in the config folder"
 
-set -e
-VAULT_IP=$(ip -o -4 addr list eth0 | head -n1 | awk '{print $4}' | cut -d/ -f1)
+export VAULT_IP=$(ip -o -4 addr list eth0 | head -n1 | awk '{print $4}' | cut -d/ -f1)
 export VAULT_API_ADDR="http://${VAULT_IP}:8200" VAULT_CLUSTER_ADDR="https://${VAULT_IP}:8201"
 log_detail "Vault IP: ${VAULT_IP}"
 log_detail "Vault API URL: ${VAULT_API_ADDR}"
 log_detail "Vault Cluster URL: ${VAULT_CLUSTER_ADDR}"
+
+cat /vault/templates/vault.json | envsubst > /vault/config/vault.json
 
 docker-entrypoint.sh server -config=/vault/config
