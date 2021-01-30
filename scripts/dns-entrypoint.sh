91de3643227e67630da25fea9a22d797c7dcc863
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-30 11:47:06 -0800

    DNS configuration is dynamically generated from Consul deployment using consul_template (Not Tested)

--- scripts/dns-entrypoint.sh
+++ scripts/dns-entrypoint.sh
@@ -4,21 +4,11 @@
 source "${CORE_SCRIPT_DIR}"/common-functions.sh
 add_path "${CORE_SCRIPT_DIR}"
 
-# Update existing packages
-apk update
-
 # Add required packages
-apk add --no-cache \
-  curl \
-  jq \
-  iputils \
-  iproute2 \
-  bind-tools \
-  gettext \
-  openssl \
-  lshw
+download_required_packages
 
-# Add Consul Template Processor
+# Add Consul & Consul Template Processor
+download_consul
 download_consul_template
 
 log "Looking up the IP address for Consul to set as Consul domain owner"
@@ -32,9 +22,11 @@ while [ -z "${CONSUL_IP}" ]; do
   export CONSUL_IP=$(get_ip_from_name "consul.service.consul")
   set -e
 done
+export CONSUL_HTTP_ADDR=http://${CONSUL_IP}:8500
 
-log_detail "merging expanded variables with configuration templates and placing in the config folder"
-cat /etc/templates/dnsmasq.conf | envsubst > /etc/dnsmasq/dnsmasq.conf
+log_detail "updating configuration based on Consul cluster deployment"
+#cat /etc/templates/dnsmasq.conf | envsubst > /etc/dnsmasq/dnsmasq.conf
+run_consul_template /etc/templates/dnsmasq.conf dnsmasq.conf /etc/dnsmasq/dnsmasq.conf "consul lock -http-addr=${CONSUL_HTTP_ADDR} -name=service/dnsmasq -shell=false restart killall dnsmasq"
 
 # Get Docker/Node/Hosting information from the Docker API for use in configuration
 hosting_details
@@ -42,5 +34,6 @@ log "-----------------------------------------------------------"
 log "- DNS Details"
 log "-----------------------------------------------------------"
 log_detail "${CONSUL_DOMAIN} domain downstream DNS: ${CONSUL_IP}"
+log_detail "Consul HTTP Address: ${CONSUL_HTTP_ADDR}"
 
 dnsmasq --no-daemon --log-queries --server=/"${CONSUL_DOMAIN}"/"${CONSUL_IP}"#8600
