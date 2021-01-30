e8574e0ef9e7a2e736e502ada65be36e0b798376
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-29 20:01:23 -0800

    zdfdzfgdf

--- scripts/nginx-entrypoint.sh
+++ scripts/nginx-entrypoint.sh
@@ -5,12 +5,16 @@ source "${CORE_SCRIPT_DIR}"/common_functions.sh
 add_path "${CORE_SCRIPT_DIR}"
 
 # Update existing packages
-apk update
+apt-get update
 
 # Add required packages
-apk add --no-cache \
+apt-get -y --no-install-recommends install \
+  unzip \
   curl \
-  jq \
+  ca-certificates \
+  procps \
+  less \
+  vimjq \
   iputils \
   iproute2 \
   bind-tools \
@@ -20,15 +24,11 @@ apk add --no-cache \
 
 # this command will automatically register the portal app as a consul service
 set -ex
-type curl || (
-  until apt-get update; do sleep 3; done
-  until apt-get -y --no-install-recommends install unzip curl ca-certificates procps less vim; do sleep 3; done
-)
 curl -fLo /usr/share/nginx/html/stylesheet.css https://raw.githubusercontent.com/samrocketman/jervis-api/gh-pages/1.6/stylesheet.css
 curl -fLo ~/.vimrc https://raw.githubusercontent.com/samrocketman/home/master/dotfiles/.vimrc
-consul-agent.sh --service '{"service": {"name": "portal", "tags": [], "port": 80}}' \
-  --consul-template-file-cmd /nginx.conf nginx.tpl /etc/nginx/conf.d/default.conf "consul lock -name service/portal -shell=false reload nginx -s reload" \
-  --consul-template-file /index.html index.html.tpl /usr/share/nginx/html/index.html
+# consul-agent.sh --service '{"service": {"name": "portal", "tags": [], "port": 80}}' \
+#   --consul-template-file-cmd /nginx.conf nginx.tpl /etc/nginx/conf.d/default.conf "consul lock -name service/portal -shell=false reload nginx -s reload" \
+#   --consul-template-file /index.html index.html.tpl /usr/share/nginx/html/index.html
 
 # Get Docker/Node/Hosting information from the Docker API for use in configuration
 hosting_details
--- scripts/vault-entrypoint.sh
+++ scripts/vault-entrypoint.sh
@@ -3,25 +3,31 @@
 # Make our stuff available
 source "${CORE_SCRIPT_DIR}"/common_functions.sh
 add_path "${CORE_SCRIPT_DIR}"
-
+set +e
 # Update existing packages
 apk update
 
 # Add required packages
 apk add --no-cache \
+  curl \
+  jq \
   iputils \
   iproute2 \
   bind-tools \
-  gettext
+  gettext \
+  openssl \
+  lshw
 
-set -elog_detail "merging expanded variables with configuration templates and placing in the config folder"
 
+# Get Docker/Node/Hosting information from the Docker API for use in configuration
+hosting_details
 export VAULT_IP=$(ip -o -4 addr list eth0 | head -n1 | awk '{print $4}' | cut -d/ -f1)
 export VAULT_API_ADDR="http://${VAULT_IP}:8200" VAULT_CLUSTER_ADDR="https://${VAULT_IP}:8201"
 log_detail "Vault IP: ${VAULT_IP}"
 log_detail "Vault API URL: ${VAULT_API_ADDR}"
 log_detail "Vault Cluster URL: ${VAULT_CLUSTER_ADDR}"
 
+log_detail "merging expanded variables with configuration templates and placing in the config folder"
 cat /vault/templates/vault.json | envsubst > /vault/config/vault.json
 
-docker-entrypoint.sh server -config=/vault/config
+docker-entrypoint.sh "$@"
