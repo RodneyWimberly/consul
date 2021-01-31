5160a86237b17f24e4ac4341ad0918fa817c505f
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-30 19:57:04 -0800

    Portal configuration now generated via consul_template New functions to add, remove, get, list, and set maintenance mode for Consul services

--- scripts/nginx-entrypoint.sh
+++ scripts/nginx-entrypoint.sh
@@ -32,9 +32,6 @@ download_consul_template
 # Download StyleSheet and Vim startup configuration
 curl -fLo /usr/share/nginx/html/stylesheet.css https://raw.githubusercontent.com/samrocketman/jervis-api/gh-pages/1.6/stylesheet.css
 curl -fLo ~/.vimrc https://raw.githubusercontent.com/samrocketman/home/master/dotfiles/.vimrc
-# consul-agent.sh --service '{"service": {"name": "portal", "tags": [], "port": 80}}' \
-#   --consul-template-file-cmd /nginx.conf nginx.tpl /etc/nginx/conf.d/default.conf "consul lock -name service/portal -shell=false reload nginx -s reload" \
-#   --consul-template-file /index.html index.html.tpl /usr/share/nginx/html/index.html
 
 export CONSUL_IP=$(get_ip_from_name "consul.service.consul")
 export CONSUL_HTTP_ADDR=http://${CONSUL_IP}:8500
@@ -46,5 +43,8 @@ log "- Portal Details"
 log "-----------------------------------------------------------"
 log_detail "Consul Address: ${CONSUL_IP}"
 log_detail "Consul HTTP Address: ${CONSUL_HTTP_ADDR}"
+
+run_consul_template /etc/templates/nginx.conf nginx.conf /etc/nginx/conf.d/default.conf "consul lock -name service/portal -shell=false reload nginx -s reload"
+run_consul_template /etc/templates/index.html index.html /usr/share/nginx/html/index.html
 
 exec nginx -g 'daemon off;'
